2f9dc63c7eabf5cbbd4975d9da8619ba3f9ac5a0
Guilherme Paterlini <guilhermepaterlini@gmail.com>
2023-11-29 23:05:19 -0400

    criando primeiro componente e preparando ambiente para o projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-function App() {
-  return <h1>Hello World!</h1>;
-}
+import { Post } from "./Post";
 
-export default App;
+export function App() {
+  return (
+    <div>
+      <Post
+        author="Paterlini"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, quos. Esse voluptates distinctio illo. Perspiciatis impedit eligendi itaque ea sed."
+      />
+      <Post
+        author="Guilherme"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, quos. Esse voluptates distinctio illo. Perspiciatis impedit eligendi itaque ea sed."
+      />
+    </div>
+  );
+}
