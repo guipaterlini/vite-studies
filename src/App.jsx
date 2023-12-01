30cc78996d824cbfd14d16c281d57e57147ab2e7
Guilherme Paterlini <guilhermepaterlini@gmail.com>
2023-11-30 22:20:58 -0400

    criado componente header e estabelecido o uso de css modules no proejto

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { Post } from "./Post";
+import { Header } from "./components/Header";
+
+import "./global.css";
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Paterlini"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, quos. Esse voluptates distinctio illo. Perspiciatis impedit eligendi itaque ea sed."
