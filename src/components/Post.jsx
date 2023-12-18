248990c62d578c7b994901c0c8bf58d76f219396
Guilherme Paterlini <guilhermepaterlini@gmail.com>
2023-12-17 20:54:53 -0400

    Criado o componente Avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,6 @@
+import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
+
 import styles from "./Post.module.css";
 
 export function Post() {
@@ -6,10 +8,8 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/guipaterlini.png"
-          />
+          <Avatar src="https://github.com/guipaterlini.png" />
+
           <div className={styles.authorInfo}>
             <strong>Guilherme Paterlini</strong>
             <span>Web Developer</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { PencilLine } from "@phosphor-icons/react";
 
 import styles from "./Sidebar.module.css";
+import { Avatar } from "./Avatar";
 
 export function Sidebar() {
   return (
@@ -11,10 +12,8 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/guipaterlini.png"
-        />
+        <Avatar src="https://github.com/guipaterlini.png" />
+
         <strong>Guilherme Paterlini</strong>
         <span>Web Developer</span>
       </div>
