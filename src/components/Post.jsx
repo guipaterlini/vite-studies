473f04b45fb95f79f4dcbf08334b608282f609fe
Guilherme Paterlini <guilhermepaterlini@gmail.com>
2024-01-11 20:58:44 -0400

    Feita a comunicação entre Post Comment para a função de deletar comentario

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -35,6 +35,10 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText(event.target.value);
   }
 
+  function deleteComment(comment) {
+    console.log(`deltar comentario ${comment}`);
+  }
+
   return (
     <article className={styles.post}>
       <header>
@@ -85,7 +89,13 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map((comment) => {
-          return <Comment key={comment} content={comment} />;
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          );
         })}
       </div>
     </article>
